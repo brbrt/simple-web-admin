4a564322a21ddf127bcc03bc2a30dece90f3e978
rbr8 <bencsr@freemail.hu>
2015-06-17 19:30:48 +0200

    Add simple command sender html page

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 var express = require('express');
+var expressHbs = require('express-handlebars');
 var log = require('winston');
 var commands = require('./commands.js');
 
 var app = express();
 
-app.get('/', function (req, resp) {
-  resp.send('Hello Pi!');
+app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'main.hbs'}));
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res){
+    var data = {commands: commands.getAll()};
+    res.render('commands', data);
 });
 
 app.get('/command', function (req, resp) {
-  resp.json(commands.getAll());
+    resp.json(commands.getAll());
 });
 
 app.post('/command/:name', function (req, resp) {
     commands
         .execute(req.params.name)
         .then(function success(output) {
+            resp.header('Content-Type', 'text/plain');
             resp.send(output);
         })
         .catch(function error(err) {
+            resp.header('Content-Type', 'text/plain');
             resp.status(500);
             resp.send(err);
         });
